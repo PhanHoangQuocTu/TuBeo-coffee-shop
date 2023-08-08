66eeece0bc52caacdfdb76e7242f1f8073304413
Tu <tuphan694@gmail.com>
2023-08-08 09:07:16 +0700

    fix

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,13 +2,17 @@ const mongoose = require('mongoose')
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb+srv://tubeocoffee:tubeocoffee@tbcoffee.iovvrey.mongodb.net/?retryWrites=true&w=majority')
-        console.log('connect to database success !!!')
-    }
-    catch {
-        console.log('connect to database failure !!!')
+        mongoose.set('strictQuery', false);
+
+        await mongoose.connect('mongodb+srv://tubeocoffee:tubeocoffee@tbcoffee.iovvrey.mongodb.net/?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log('Connected to the database successfully!');
+    } catch (error) {
+        console.error('Failed to connect to the database:', error);
     }
 }
-
 
 module.exports = { connect }
